beb3eabd22f85b559823471b0f0cff47228327a8
HazafiLevente <hazafilev@gmail.com>
2025-09-07 11:06:27 +0200

    Home.ts

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname)));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, ''));
+    res.sendFile(path.join(__dirname, 'Home.ts'));
 });
 
 app.listen(PORT, () => {
