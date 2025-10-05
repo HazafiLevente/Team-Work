8f062f2099e5e617f5d120e9d2e58197b0aaa105
HazafiLevente <hazafilev@gmail.com>
2025-10-05 10:10:57 +0200

    Add datas folder with guitars.sql

---
+++ datas/musical instrument/guitars.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE IF NOT EXISTS datas;
+USE datas;
+
+CREATE TABLE IF NOT EXISTS Guitar (
+    GuitarID int,
+    Name varchar(255),
+    Modell varchar(255),
+    Type varchar(255),
+    Color varchar(255),
+    Body varchar(255),
+    Neck varchar(255),
+    Fretboard varchar(255),
+    Headstock varchar(255),
+    TuningPegs varchar(255),
+    Nut varchar(255),
+    Bridge varchar(255),
+    Pickups varchar(255),
+    Pickguard varchar(255),
+    Strings varchar(255),
+    Controls varchar(255),
+    StrapButtons varchar(255)
+);
+
+        
