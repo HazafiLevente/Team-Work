72663c1c027396a9104f185b31312597cd64d7b4
Hazafi Levente <hazafilev@gmail.com>
2026-01-26 13:11:15 +0100

    Angular része

---
+++ Setup-Configurator/src/app/Components/Home/searchbar/searchbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './searchbar.component.html',
+  styleUrl: './searchbar.component.css'
+})
+export class HeaderComponent {
+  public searchButton(){
+
+  }
+}
