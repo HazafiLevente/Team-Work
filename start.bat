98b70d56c7a180d40f73db660d96a40692b1a5df
HazafiLevente <hazafilev@gmail.com>
2026-03-14 11:17:51 +0100

    control.js megoldása

--- start.bat
+++ start.bat
@@ -2,12 +2,29 @@
 title Projekt Dev Starter
 
 echo =====================================
+echo Checking Node.js...
+echo =====================================
+
+node -v >nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+    echo Node.js not found. Downloading...
+
+    powershell -Command "Invoke-WebRequest https://nodejs.org/dist/v22.12.0/node-v22.12.0-x64.msi -OutFile nodejs.msi"
+
+    echo Installing Node.js...
+    msiexec /i nodejs.msi /quiet /norestart
+
+    echo Node installed.
+) ELSE (
+    echo Node.js already installed.
+)
+
+echo =====================================
 echo Installing dependencies...
 echo =====================================
 
 cd backend
 call npm install
-
 cd ..
 
 cd Setup-Configurator
@@ -21,7 +38,7 @@ echo =====================================
 start cmd /k "cd backend && node server.js"
 
 echo =====================================
-echo Starting Angular Server (port 80)...
+echo Starting Angular Server...
 echo =====================================
 
 start cmd /k "cd Setup-Configurator && npm run start:proxy"
